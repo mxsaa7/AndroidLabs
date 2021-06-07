895fee13260621a6e4ccb8c642e904f6b0f19965
mxsaa7 <garr0118@algonquinlive.com>
2021-06-07 18:51:39 -0500

    Lab_3 git

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -2,41 +2,76 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String ACTIVITY_NAME = "ProfileActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-    }
-    @Override
-    protected void onStart(){ super.onStart(); }
 
-    @Override
-    protected void onPause(){ super.onPause(); }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
+        // When the picture button is clicked, start the camera intent
+        ((ImageButton) findViewById(R.id.profile_picture)).setOnClickListener(clk -> {
+            dispatchTakePictureIntent();
+        });
     }
 
-    @Override
-    protected void onStop(){
-        super.onStop();
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-        Bundle extras = data.getExtras();
-        Bitmap imageBitmap = (Bitmap) extras.get("data");
-        mImageButton.setImageBitmap(imageBitmap);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ((ImageButton) findViewById(R.id.profile_picture)).setImageBitmap(imageBitmap);
         }
+        Log.e(ACTIVITY_NAME, "in function" + "onActivityResult();");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "in function" + "onStart();");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "in function" + "onPause();");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "in function" + "onResume();");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "in function" + "onStop;");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "in function" + "onDestroy();");
     }
 }
